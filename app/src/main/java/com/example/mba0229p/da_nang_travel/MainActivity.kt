a24ca6eb3ae9ee41d0f789449a2eef6d07640f73
hieu.cap <mba0229p@MBA0229P.local>
2018-10-11 20:17:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mba0229p/da_nang_travel/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mba0229p.da_nang_travel
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
