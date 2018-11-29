3c481b84ff25620895a358354f258636337bc01d
hieu.cap <mba0229p@MBA0229P.local>
2018-11-29 21:28:52 +0700

    format code

--- app/src/main/java/com/example/mba0229p/da_nang_travel/data/source/local/LocalDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mba0229p.da_nang_travel.data.source
-
-import android.content.Context
-
-
-class LocalDataSource(context: Context) {
-}
--- app/src/main/java/com/example/mba0229p/da_nang_travel/data/source/local/sharedPref/SharedPrefDataSourceImpl.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mba0229p.da_nang_travel.data.source.local.sharedPref
-
-import com.example.mba0229p.da_nang_travel.data.source.datasource.SharedPrefDataSource
-
-class SharedPrefDataSourceImpl : SharedPrefDataSource {
-}
